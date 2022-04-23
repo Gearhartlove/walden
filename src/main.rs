b2264cc0b6ca0a2d6a5a16051ab6205b09a58c24
Kristoff <finley@kristoff.codes>
2022-04-23 11:08:57 -0600

    CardPlugin work. Prototype of Card Creation completed.

--- src/main.rs
+++ src/main.rs
@@ -13,48 +13,88 @@ pub struct CardPlugin;
 
 impl Plugin for CardPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(hello_world);
-    }
-}
 
-fn hello_world() {
-    println!("hello walden!")
+    }
 }
 
 // entity
 #[derive(Component, Debug)]
-pub struct Card(String);
+pub struct Card;
 
 #[derive(Component)]
-struct HasPower(usize);
+pub struct Name(String);
 
 #[derive(Component)]
-struct HasHealth(usize);
+struct Power(usize);
 
-//system
-// fn print_cards(query: Query<&Card, With<Information>>) {
-//     for q in query.iter() {
-//         println!("card: {:?}", q)
-//     }
-// }
-fn print_cards_names(query: Query<&Card>) {
+#[derive(Component)]
+struct Health(usize);
+
+#[derive(Component)]
+struct Description(String);
+
+#[derive(Component)]
+// idea: could make a custom parser for the ability of the
+// card which would allow users to write their own cards
+// and play with them
+struct Ability(String); // make ability a closure // complicated behavior
+struct Keywords(Vec<String>);
+
+#[derive(Component)]
+struct Rarity(RareQualities);
+enum RareQualities {
+    Common,
+    Rare,
+    Epic,
+    Legendary,
+}
+
+#[derive(Component)]
+struct CardTypes;
+// enum CardTypes {} // Sub-card types
+
+#[derive(Component)]
+// idea: write a ai generated card art which is inspired by
+// the name and abilities of the card
+struct Art(String); // string is a path to the card art
+
+#[derive(Component)]
+struct ResourceCost(usize);
+
+fn print_cards_names(query: Query<&Name, With<Card>>) {
     for card in query.iter() {
         println!("{}", card.0)
     }
 }
 
-//////////
 fn add_cards(mut commands: Commands) {
     commands.spawn_batch(vec![
         (
-            Card("Phoenix".to_string()),
-            HasPower(7),
-            HasHealth(3),
+            Card,
+            Name("Clump of Whumps".to_string()),
+            Power(2),
+            Health(2),
+            Ability("When I'm Summoned, create a Mushroom Cloud in hand.".to_string()),
+            Rarity(RareQualities::Common),
+            ResourceCost(2),
+        ),
+        (   Card,
+            Name("Leeroy Jenkins".to_string()),
+            Power(6),
+            Health(2),
+            Ability("Summon two 1/1 Whealps for your opponent".to_string()),
+            Rarity(RareQualities::Legendary),
+            ResourceCost(5),
+            Keywords(vec!("Charge".to_string(), "Battlecry".to_string())),
+            Art("path to art on computer".to_string()),
         ),
         (
-            Card("Turtle".to_string()),
-            HasPower(2),
-            HasHealth(9),
+            Card,
+            Name("Llanowar Elves".to_string()),
+            Power(1),
+            Health(1),
+            Description("As patient and generous as life, as harsh
+            and merciless as nature".to_string()),
         ),
     ]);
 }
